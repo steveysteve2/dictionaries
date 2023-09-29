31358bc855b8948eb8f4c6031a0d613cab448a92
Andrew Stevens <Andrew_Stevens1@baylor.edu>
2023-09-28 22:44:34 -0500

    schools report

--- 7_schools_report.py
+++ 7_schools_report.py
@@ -10,12 +10,6 @@ Copy that info here:
 "NCAA/NAIA conference number football (IC2020)","107","Big Ten Conference"
 "NCAA/NAIA conference number football (IC2020)","130","Southeastern Conference"
 
-
-Display report for all universities that have a graduation rate for Women over 75%
-Display report for all universities that have a total price for in-state students living off campus over $50,000
-
-
-
 """
 
 import json
@@ -25,14 +19,28 @@ schools = json.load(infile)     #loads a list of the json file
 
 conference_schools = [372,108,107,130]
 
-print("Total Num of Schools: ", len(schools))
+#Display schools in ACC, Big 12, Big Ten, and SEC
+for school in schools:
+    if school['NCAA']["NAIA conference number football (IC2020)"] in conference_schools:
+        print(school["instnm"])
 
-# for school in schools:
-#     if school['NCAA']["NAIA conference number football (IC2020)"] in conference_schools:
-#         print(school["instnm"])
+# Display report for all universities that have a graduation rate for Women over 75%
+
+st1 = "Graduation rate  women (DRVGR2020)"
 
 for school in schools:
-    if (school["Graduation rate  women (DRVGR2020)"]) == int and (school["Graduation rate  women (DRVGR2020)"]) > 75:
+    if (school[st1]) != None and (school[st1]) > 75:
         print(f"School: {school['instnm']}")
-        print(f'Women Grad Rate: {school["Graduation rate  women (DRVGR2020)"]}')
+        print(f'Women Grad Rate: {school[st1]}')
 
+# Display report for all universities that have a total price for 
+# in-state students living off campus over $50,000
+
+st = "Total price for in-state students living off campus (not with family)  2020-21 (DRVIC2020)"
+
+for school in schools:
+    if (school[st]) != None and (school[st]) > 50000:
+        print(f"School: {school['instnm']}")
+        print(f'Price for students in-state living off-campus: {school[st]}')
+
+infile.close()
